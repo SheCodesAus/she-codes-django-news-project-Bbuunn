cf8c450d4355985d5604bfaf8570c115c5ad2f62
Bbuunn <bunnyryou@gmail.com>
2022-12-09 20:36:50 +1100

    Order the stories by date

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -2,20 +2,22 @@ from django.views import generic
 from django.urls import reverse_lazy
 from .models import NewsStory
 from .forms import StoryForm
+from django.shortcuts import render # 2022-12-09
+
 
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
-
+    
     def get_queryset(self):
         '''Return all news stories.'''
-        return NewsStory.objects.all()
+        # previously return NewsStory.objects.all()
+        return NewsStory.objects.order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
+        context['latest_stories'] = self.get_queryset()[:4]
+        context['all_stories'] = self.get_queryset()
         return context
-
 class StoryView(generic.DetailView):
     model = NewsStory
     template_name = 'news/story.html'
@@ -29,4 +31,5 @@ class AddStoryView(generic.CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
