e0d48099e0ecfe15065125da8418ecfc228e5cad
Bbuunn <bunnyryou@gmail.com>
2022-12-11 16:10:47 +1100

    Change CSS - In Progress

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'news'
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
     path('add-story/', views.AddStoryView.as_view(), name='addStory'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
